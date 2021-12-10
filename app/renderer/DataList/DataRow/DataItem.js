253a9e0f0772827465fffea922fe40e3f3e56315
ninjaboynaru <thiagoCord755@gmial.com>
2021-12-10 13:32:35 -0500

    Location field validation

--- app/renderer/DataList/DataRow/DataItem.js
+++ app/renderer/DataList/DataRow/DataItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function DataItem({ children, label }) {
+function DataItem({ children, label, error }) {
+	let errorUI = null;
+
+	if (error) {
+		errorUI = <p className="datalist-row__item-error">{error}</p>;
+	}
+
 	return (
 		<div className="datalist-row__item">
 			<p className="datalist-row__item-label">{label}</p>
 			<div className="datalist-row__item-content">
 				{children}
+				{errorUI}
 			</div>
 		</div>
 	);
--- app/renderer/LocationsScreen/LocationEditDataRow.js
+++ app/renderer/LocationsScreen/LocationEditDataRow.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { faSave, faUndo } from '@fortawesome/free-solid-svg-icons';
 import { DataRow, DataItem, DataRowTextInput, DataRowButton } from '../DataList';
 
+const MAX_NAME_LENGTH = 100;
+
 class LocationEditDataRow extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { name: this.props.location.name };
+		this.state = { name: this.props.location.name, nameError: null };
 		this.modifiedLocation = { ...this.props.location };
 		this.onNameChange = this.onNameChange.bind(this);
 		this.onSave = this.onSave.bind(this);
@@ -18,13 +20,29 @@ class LocationEditDataRow extends React.Component {
 	}
 
 	onSave() {
+		const name = this.state.name.trim();
+		let nameError = null;
+
+		if (name.length === 0) {
+			nameError = 'Name is too short';
+		}
+		else if (name.length >= MAX_NAME_LENGTH) {
+			nameError = 'Name is too long';
+		}
+
+		if (nameError) {
+			this.setState({ nameError });
+			return;
+		}
+
 		this.props.onSaveClick(this.modifiedLocation);
 	}
 
 	render() {
+		const { name, nameError } = this.state;
 		return (
 			<DataRow>
-				<DataItem label="Name"><DataRowTextInput value={this.state.name} placeholder="Name" onChange={this.onNameChange} /></DataItem>
+				<DataItem label="Name" error={nameError}><DataRowTextInput value={name} placeholder="Name" onChange={this.onNameChange} /></DataItem>
 				<DataRowButton icon={faSave} onClick={this.onSave} />
 				<DataRowButton icon={faUndo} onClick={this.props.onCancelClick} outline />
 			</DataRow>
